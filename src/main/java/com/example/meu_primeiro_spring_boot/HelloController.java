9501ff1d4ce6d68a774eb853f31d70f6bfb7e2d4
Agusto César <negocios.augusto@gmail.com>
2026-08-18 13:49:35 -0300

    Olá mundo executado com sucesso no navegador.

---
+++ src/main/java/com/example/meu_primeiro_spring_boot/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.meu_primeiro_spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Olá mundo! Bem vindo ao Spring Boot";
+  }
+
+}
