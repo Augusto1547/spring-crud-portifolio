14592905c56a73a646227879f1161e2af4bcb204
Agusto César <negocios.augusto@gmail.com>
2026-08-19 14:17:45 -0300

    Criar o método buscarPorId dentro de ProdutoService

--- src/main/java/com/example/meu_primeiro_spring_boot/service/ProdutoService.java
+++ src/main/java/com/example/meu_primeiro_spring_boot/service/ProdutoService.java
@@ -1,6 +1,7 @@
 package com.example.meu_primeiro_spring_boot.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,10 @@ public class ProdutoService {
 
   public List<Produto> listarProdutos() {
     return produtoRepository.findAll();
+  }
+
+  public Optional<Produto> buscarPorId(Long id) {
+    return produtoRepository.findById(id);
   }
 
 }
