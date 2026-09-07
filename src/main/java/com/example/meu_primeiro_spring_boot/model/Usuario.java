b80914f36222fec992b1f78f9ec69b92d4269535
Agusto César <negocios.augusto@gmail.com>
2026-09-07 16:46:42 -0300

    Criar a entidade Usuario para representar a validação do usuário

---
+++ src/main/java/com/example/meu_primeiro_spring_boot/model/Usuario.java
@@ -0,0 +1,58 @@
+package com.example.meu_primeiro_spring_boot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String passWord;
+
+    public Usuario() {
+    }
+
+    public Usuario(String passWord, String userName) {
+        this.passWord = passWord;
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+
+
+}
