dc823c2d9e186c2e0bfb7e646135c38e0ac626b5
Agusto César <negocios.augusto@gmail.com>
2026-09-10 15:19:54 -0300

    Criar a classe AuthController que recebe as requisições para autenticar usuário e responder token jwt

--- src/main/java/com/example/meu_primeiro_spring_boot/controller/AuthController.java
+++ src/main/java/com/example/meu_primeiro_spring_boot/controller/AuthController.java
@@ -1,17 +1,42 @@
 package com.example.meu_primeiro_spring_boot.controller;
 
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.meu_primeiro_spring_boot.model.Usuario;
+import com.example.meu_primeiro_spring_boot.security.JwtUtil;
+import com.example.meu_primeiro_spring_boot.service.UsuarioService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    
-  private final UsuarioService usuarioService;
 
-  public AuthController(UsuarioService usuarioService){
+    private final UsuarioService usuarioService;
 
-    this.usuarioService = usuarioService;
-  }
+    public AuthController(UsuarioService usuarioService) {
 
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request) {
+        Usuario usuario = usuarioService.registrarUsuario(request.get("username"), "password");
+        return ResponseEntity.ok(usuario);//poderia pasar body ou objeto
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        Optional<Usuario> usuario = usuarioService.buscarPorUsername(request.get("username"));
+        if (usuario.isPresent() && usuario.get().getPassWord().equals(request.get("password"))) {
+            String token = JwtUtil.generateToken(usuario.get().getUserName());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body("Credenciais inválidas");
+    }
 }
